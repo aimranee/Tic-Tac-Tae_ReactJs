a38c691f71f2263d30de09892486a967708896a1
aimranee <aimrane.essakhi@gmail.com>
2023-04-10 09:05:44 +0000

    update

---
+++ src/components/start/index.js
@@ -0,0 +1,45 @@
+import React, { useContext } from "react";
+
+import { GameContext } from "../../context/GameContex";
+import Oicon from "../icons/Oicon";
+import Xicon from "../icons/Xicon";
+
+const Start = () => {
+  const { activeUser, setActiveUser, handleStart } = useContext(GameContext);
+
+  return (
+    <div className="start">
+      <div className="card shadow-gray">
+        <h1 className="text-lg">Choisissez le symbole du joueur</h1>
+        <div className="start__players">
+          <span
+            className={activeUser === "x" ? "start__players--active" : ""}
+            onClick={() => setActiveUser("x")}
+          >
+            <Xicon color={activeUser === "x" ? "dark" : "light"} />
+          </span>
+          <span
+            className={activeUser === "o" ? "start__players--active" : ""}
+            onClick={() => setActiveUser("o")}
+          >
+            <Oicon color={activeUser === "o" ? "dark" : "light"} />
+          </span>
+        </div>
+        <p className="text-light text-normal">
+          rappelez-vous : x passe en premier
+        </p>
+      </div>
+      <div className="start__btns">
+        <button className="btn btn-yellow" onClick={() => handleStart("cpu")}>
+          Jouer avec l'ordinateur
+        </button>
+        <button className="btn btn-blue" onClick={() => handleStart("user")}>
+          {" "}
+          1 VS 1
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Start;
